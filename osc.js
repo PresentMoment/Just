18afd3d1d8dd0499c64f5bb87e0920a441157f65
Robert Girardin <robertgirardin@Roberts-MacBook-Pro.local>
2019-06-20 11:00:46 +0200

    fourth commit

--- osc.js
+++ osc.js
@@ -48,6 +48,10 @@ let height = 790;
 
 const note = new Notes();
 
+const label = new Labels();
+
+const settings = new OscSettings();
+
 function setup() {
   createCanvas(width, height);
   noStroke();
@@ -151,267 +155,16 @@ function setup() {
 function draw() {
   background(23,63,80);
 
-  //set the freqencies for the oscs
-  osc1.freq(just[0]);
-  osc2.freq(just[1]);
-  osc3.freq(just[2]);
-  osc4.freq(just[3]);
-  osc5.freq(just[4]);
-  osc6.freq(just[5]);
-  osc7.freq(just[6]);
-  osc8.freq(just[7]);
-  osc9.freq(just[8]);
-  osc10.freq(just[9]);
-  osc11.freq(just[10]);
-  osc12.freq(just[11]);
+  settings.soundSettings();
 
-  //set the phase of the oscs
-  osc1.phase(oscPhase[0]);
-  osc2.phase(oscPhase[1]);
-  osc3.phase(oscPhase[2]);
-  osc4.phase(oscPhase[3]);
-  osc5.phase(oscPhase[4]);
-  osc6.phase(oscPhase[5]);
-  osc7.phase(oscPhase[6]);
-  osc8.phase(oscPhase[7]);
-  osc9.phase(oscPhase[8]);
-  osc10.phase(oscPhase[9]);
-  osc11.phase(oscPhase[10]);
-  osc12.phase(oscPhase[11]);
-
-  //change the osc waveforms
-  osc1.setType(oscTypes[0]);
-  osc2.setType(oscTypes[0]);
-  osc3.setType(oscTypes[0]);
-  osc4.setType(oscTypes[0]);
-  osc5.setType(oscTypes[0]);
-  osc6.setType(oscTypes[0]);
-  osc7.setType(oscTypes[0]);
-  osc8.setType(oscTypes[0]);
-  osc9.setType(oscTypes[0]);
-  osc10.setType(oscTypes[0]);
-  osc11.setType(oscTypes[0]);
-  osc12.setType(oscTypes[0]);
-
-  //draw the intonation buttons
-  fill(14,43,53);
-  rect(10, 10, 300, 50);
-  fill(255);
-  text('7 Limit Tritone', 70, 20, 300, 50);
-  fill(14,43,53);
-  rect(320, 10, 300, 50);
-  fill(255);
-  text('7 Limit 12-Tone', 390, 20, 300, 50);
-  fill(14,43,53);
-  rect(630, 10, 300, 50);
-  fill(255);
-  text('Alternate 12-Tone', 680, 20, 300, 50);
-  fill(14,43,53);
-  rect(940, 10, 300, 50);
-  fill(255);
-  text('12-Tone Subharmonic', 960, 20, 300, 50);
-
-  //draw the intonation reset button
-  fill(14,43,53);
-  rect(1250, 10, 70, 50);
-  fill(255);
-  textSize(22);
-  textStyle(ITALIC);
-  text('reset', 1258, 23, 70, 50);
-  textSize(27);
-  textStyle(NORMAL);
-
-  //draw the osc buttons
-  fill(14,43,53);
-  rect(10, 600, 100, 50);
-  fill(255);
-  text('Osc 1', 20, 610, 100, 50);
-  fill(14,43,53);
-  rect(120, 600, 100, 50);
-  fill(255);
-  text('Osc 2', 130, 610, 100, 50);
-  fill(14,43,53);
-  rect(230, 600, 100, 50);
-  fill(255);
-  text('Osc 3', 240, 610, 100, 50);
-  fill(14,43,53);
-  rect(340, 600, 100, 50);
-  fill(255);
-  text('Osc 4', 350, 610, 100, 50);
-  fill(14,43,53);
-  rect(450, 600, 100, 50);
-  fill(255);
-  text('Osc 5', 460, 610, 100, 50);
-  fill(14,43,53);
-  rect(560, 600, 100, 50);
-  fill(255);
-  text('Osc 6', 570, 610, 100, 50);
-  fill(14,43,53);
-  rect(670, 600, 100, 50);
-  fill(255);
-  text('Osc 7', 680, 610, 100, 50);
-  fill(14,43,53);
-  rect(780, 600, 100, 50);
-  fill(255);
-  text('Osc 8', 790, 610, 100, 50);
-  fill(14,43,53);
-  rect(890, 600, 100, 50);
-  fill(255);
-  text('Osc 9', 900, 610, 100, 50);
-  fill(14,43,53);
-  rect(1000, 600, 100, 50);
-  fill(255);
-  text('Osc 10', 1010, 610, 100, 50);
-  fill(14,43,53);
-  rect(1110, 600, 100, 50);
-  fill(255);
-  text('Osc 11', 1120, 610, 100, 50);
-  fill(14,43,53);
-  rect(1220, 600, 100, 50);
-  fill(255);
-  text('Osc 12', 1230, 610, 100, 50);
-
-
-   //draw the note labels
-   if (osc1.started == true){
-     fill(14,43,53)
-   } else {fill(19,55,69)}
-   rect(10, 720, 100, 50);
-   fill(255);
-   text(note1, 20, 730, 100, 50);
-   if (osc2.started == true){
-    fill(14,43,53)
-  } else {fill(19,55,69)}
-   rect(120, 720, 100, 50);
-   fill(255);
-   text(note2, 130, 730, 100, 50);
-   if (osc3.started == true){
-    fill(14,43,53)
-  } else {fill(19,55,69)}
-   rect(230, 720, 100, 50);
-   fill(255);
-   text(note3, 240, 730, 100, 50);
-   if (osc4.started == true){
-    fill(14,43,53)
-  } else {fill(19,55,69)}
-   rect(340, 720, 100, 50);
-   fill(255);
-   text(note4, 350, 730, 100, 50);
-   if (osc5.started == true){
-    fill(14,43,53)
-  } else {fill(19,55,69)}
-   rect(450, 720, 100, 50);
-   fill(255);
-   text(note5, 460, 730, 100, 50);
-   if (osc6.started == true){
-    fill(14,43,53)
-  } else {fill(19,55,69)}
-   rect(560, 720, 100, 50);
-   fill(255);
-   text(note6, 570, 730, 100, 50);
-   if (osc7.started == true){
-    fill(14,43,53)
-  } else {fill(19,55,69)}
-   rect(670, 720, 100, 50);
-   fill(255);
-   text(note7, 680, 730, 100, 50);
-   if (osc8.started == true){
-    fill(14,43,53)
-  } else {fill(19,55,69)}
-   rect(780, 720, 100, 50);
-   fill(255);
-   text(note8, 790, 730, 100, 50);
-   if (osc9.started == true){
-    fill(14,43,53)
-  } else {fill(19,55,69)}
-   rect(890, 720, 100, 50);
-   fill(255);
-   text(note9, 900, 730, 100, 50);
-   if (osc10.started == true){
-    fill(14,43,53)
-  } else {fill(19,55,69)}
-   rect(1000, 720, 100, 50);
-   fill(255);
-   text(note10, 1010, 730, 100, 50);
-   if (osc11.started == true){
-    fill(14,43,53)
-  } else {fill(19,55,69)}
-   rect(1110, 720, 100, 50);
-   fill(255);
-   text(note11, 1120, 730, 100, 50);
-   if (osc12.started == true){
-    fill(14,43,53)
-  } else {fill(19,55,69)}
-   rect(1220, 720, 100, 50);
-   fill(255);
-   text(note12, 1230, 730, 100, 50);
-
-  //draw the octave up button
-  fill(14,43,53);
-  rect(10, 400, 210, 50);
-  fill(255);
-  text('Octave Up', 30, 412, 210, 50);
-  //draw the octave down button 
-  fill(14,43,53);
-  rect(10, 460, 210, 50);
-  fill(255);
-  text('Octave Down', 30, 472, 210, 50);
-
-  //draw the change osc button
-  textSize(23);
-  fill(12,43,53);
-  rect(10, 280, 210, 50);
-  fill(255);
-  text('Change Oscillators', 17, 293, 210, 50);
-  textSize(27);
-
-  //draw the osc display button
-  fill(12,33,53);
-  rect(230, 285, 125, 40);
-  fill(255);
-  textStyle(ITALIC);
-  textSize(22);
-  text(displayOsc, 245, 293, 210, 50);
-  if (oscTypes[0] == 'sine') {
-    displayOsc = 'Sine';
-  } else if (oscTypes[0] == 'triangle') {
-    displayOsc = 'Triangle';
-  } else if (oscTypes[0] == 'sawtooth') {
-    displayOsc = 'Sawtooth';
-  } else if (oscTypes[0] == 'square') {displayOsc = 'Square';
-}
-  textStyle(NORMAL);
-  textSize(27);
-
-  // //draw the phase shift buttons
-  // textSize(23);
-  // fill(14,43,53)
-  // rect(10, 100, 210, 50);
-  // fill(255);
-  // text('Shift Phase Up', 25, 112, 210, 50);
-  // textSize(23);
-  // fill(14,43,53)
-  // rect(10, 160, 210, 50);
-  // fill(255);
-  // text('Shift Phase Down', 25, 170, 210, 50);
-  // textSize(27);
-
-  //draw the all oscs and clear oscs buttons
-  fill(14,43,53);
-  rect((width - 220), 400, 210, 50);
-  fill(255);
-  text('Clear Notes', (width - 180), 412, 210, 50);
-  fill(14,43,53);
-  rect((width - 220), 460, 210, 50);
-  fill(255);
-  text('All Notes On', (width - 185), 472, 210, 50);
+  label.drawLabels();
 
   note.noteListener();
-
 }
 
 function touchStarted() {
   getAudioContext().resume()
+  return true;
 }
 
 function mouseClicked() {
@@ -492,22 +245,6 @@ function mouseClicked() {
       return i / 2 ; } )
     }
 
-  // //phase up
-  // if ((mouseX >= 10 && mouseX <= 220) &&
-  // (mouseY >= 100 && mouseY <= 150)) {
-  //   oscPhase = oscPhase.map(function(i){
-  //     return i + .1;
-  //   })
-  // }
-
-  // //phase down
-  // if ((mouseX >= 10 && mouseX <= 220) &&
-  // (mouseY >= 160 && mouseY <= 210)) {
-  //   oscPhase = oscPhase.map(function(i){
-  //     return i - .1;
-  //   })
-  // }
-
   //clear notes
   if ((mouseX >= (width - 220) && mouseX <= (width - 10) &&
   (mouseY >= 400 && mouseY <= 450))) {
@@ -551,3 +288,32 @@ function mouseClicked() {
   }
 
 }
+
+  // //phase up
+  // if ((mouseX >= 10 && mouseX <= 220) &&
+  // (mouseY >= 100 && mouseY <= 150)) {
+  //   oscPhase = oscPhase.map(function(i){
+  //     return i + .1;
+  //   })
+  // }
+
+  // //phase down
+  // if ((mouseX >= 10 && mouseX <= 220) &&
+  // (mouseY >= 160 && mouseY <= 210)) {
+  //   oscPhase = oscPhase.map(function(i){
+  //     return i - .1;
+  //   })
+  // }
+
+  // //draw the phase shift buttons
+  // textSize(23);
+  // fill(14,43,53)
+  // rect(10, 100, 210, 50);
+  // fill(255);
+  // text('Shift Phase Up', 25, 112, 210, 50);
+  // textSize(23);
+  // fill(14,43,53)
+  // rect(10, 160, 210, 50);
+  // fill(255);
+  // text('Shift Phase Down', 25, 170, 210, 50);
+  // textSize(27);
---
+++ oscSettings.js
@@ -0,0 +1,48 @@
+class OscSettings {
+  constructor(){}
+
+  soundSettings(){
+
+  //set the freqencies for the oscs
+  osc1.freq(just[0]);
+  osc2.freq(just[1]);
+  osc3.freq(just[2]);
+  osc4.freq(just[3]);
+  osc5.freq(just[4]);
+  osc6.freq(just[5]);
+  osc7.freq(just[6]);
+  osc8.freq(just[7]);
+  osc9.freq(just[8]);
+  osc10.freq(just[9]);
+  osc11.freq(just[10]);
+  osc12.freq(just[11]);
+
+  //set the phase of the oscs
+  osc1.phase(oscPhase[0]);
+  osc2.phase(oscPhase[1]);
+  osc3.phase(oscPhase[2]);
+  osc4.phase(oscPhase[3]);
+  osc5.phase(oscPhase[4]);
+  osc6.phase(oscPhase[5]);
+  osc7.phase(oscPhase[6]);
+  osc8.phase(oscPhase[7]);
+  osc9.phase(oscPhase[8]);
+  osc10.phase(oscPhase[9]);
+  osc11.phase(oscPhase[10]);
+  osc12.phase(oscPhase[11]);
+
+  //change the osc waveforms
+  osc1.setType(oscTypes[0]);
+  osc2.setType(oscTypes[0]);
+  osc3.setType(oscTypes[0]);
+  osc4.setType(oscTypes[0]);
+  osc5.setType(oscTypes[0]);
+  osc6.setType(oscTypes[0]);
+  osc7.setType(oscTypes[0]);
+  osc8.setType(oscTypes[0]);
+  osc9.setType(oscTypes[0]);
+  osc10.setType(oscTypes[0]);
+  osc11.setType(oscTypes[0]);
+  osc12.setType(oscTypes[0]);
+  }
+}
